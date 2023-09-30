2fa5f760cafe6caa8a81e22257f81035ea353ea5
Kumodatsu <dragonkumo@gmail.com>
2023-09-30 19:23:19 +0200

    Add common definitions

---
+++ yaytracer/src/common.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdint>
+
+namespace yay {
+  
+  using I8 = int8_t;
+  using I16 = int16_t;
+  using I32 = int32_t;
+  using I64 = int64_t;
+  using U8  = uint8_t;
+  using U16 = uint16_t;
+  using U32 = uint32_t;
+  using U64 = uint64_t;
+
+  using USize  = size_t; 
+  using UCount = size_t;
+  using UIndex = size_t;
+
+  using F32 = float;
+  using F64 = double;
+
+}
