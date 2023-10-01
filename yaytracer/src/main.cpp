8fe0ca5c1f5dfa9adfe89b7b43d3d30852c32c3b
Kumodatsu <dragonkumo@gmail.com>
2023-10-01 21:53:17 +0200

    Test ray directions

--- yaytracer/src/main.cpp
+++ yaytracer/src/main.cpp
@@ -4,13 +4,34 @@
 #include <iostream>
 #include <imgui.h>
 
+#include "math/math.hpp"
+#include "math/vector.hpp"
+
+#include "graphics/ray.hpp"
+#include "graphics/camera.hpp"
+
 int main(int, char**) {
   using namespace yay;
 
-  PixelBuffer buffer(640, 480);
+  static constexpr U32
+    width  = 960,
+    height = 540;
+  
+  PixelBuffer buffer(width, height);
   buffer.clear(colors::Blue);
-  Window window("Yaytracer", 640, 480);
+  Window window("Yaytracer", width, height);
   Renderer renderer(window);
+
+  Camera camera(width, height, 1.0f, Vector(0.0f, 0.0f, 0.0f));
+  const auto rays = camera.rays();
+  for (UIndex v = 0; v < height; ++v) {
+    for (UIndex u = 0; u < width; ++u) {
+      const auto direction = rays[u + width * v].direction;
+      const auto a = 0.5f * direction.y + 1.0f;
+      const auto color = blend(a, colors::White, Color(0.5f, 0.7f, 1.0f));
+      buffer.at(u, v) = color;
+    }
+  }
 
   while (!window.is_close_requested()) {
     renderer.begin_frame();
